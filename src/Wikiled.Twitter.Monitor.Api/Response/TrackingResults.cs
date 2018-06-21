205f8f47063e1d355986dbd48f08c4294876678e
Andrius Mudinas <keistokas@gmail.com>
2018-06-21 23:10:24 +0100

    Initialize dictionary

--- src/Wikiled.Twitter.Monitor.Api/Response/TrackingResults.cs
+++ src/Wikiled.Twitter.Monitor.Api/Response/TrackingResults.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Wikiled.Twitter.Monitor.Api.Response
 {
     public class TrackingResults
     {
+        public TrackingResults()
+        {
+            Sentiment = new Dictionary<string, SentimentResult>(StringComparer.OrdinalIgnoreCase);
+        }
+
         public string Keyword { get; set; }
 
         public int Total { get; set; }
