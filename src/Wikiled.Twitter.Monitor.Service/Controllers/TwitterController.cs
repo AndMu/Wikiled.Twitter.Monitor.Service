8f48df599e07d5cb9c5ddbfd867a702772d38016
Andrius Mudinas <andrius@wikiled.com>
2018-12-06 18:42:48 +0000

    Mess

--- src/Wikiled.Twitter.Monitor.Service/Controllers/TwitterController.cs
+++ src/Wikiled.Twitter.Monitor.Service/Controllers/TwitterController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Wikiled.Sentiment.Tracking.Logic;
 using Wikiled.Server.Core.ActionFilters;
 using Wikiled.Server.Core.Controllers;
+using Wikiled.Twitter.Monitor.Service.Requests;
 
 namespace Wikiled.Twitter.Monitor.Service.Controllers
 {
@@ -29,33 +31,66 @@ namespace Wikiled.Twitter.Monitor.Service.Controllers
                 return NoContent();
             }
 
-            var tracker = manager.Resolve(keyword, "Keyword");
-            TrackingResults result = new TrackingResults
+            return Ok(RequestSingle(keyword));
+        }
+
+        [HttpPost("sentimentAll")]
+        public IActionResult GetResultAll(string[] keywords)
+        {
+            Dictionary<string, TrackingResults> result = new Dictionary<string, TrackingResults>();
+            foreach (var keyword in keywords)
             {
-                Keyword = keyword
-            };
-            int[] steps = { 24, 12, 6, 1 };
-            foreach (int step in steps)
-            {
-                result.Sentiment[$"{step}H"] = new TrackingResult { Average = tracker.CalculateAverageRating(step), TotalMessages = tracker.Count(lastHours: step) };
+                result[keyword] = RequestSingle(keyword);
             }
 
-            result.Total = tracker.Count(false);
             return Ok(result);
         }
 
         [Route("history/{hours}/{keyword}")]
         [HttpGet]
-        public IActionResult GetResultHistory(string keyword, int hours)
+        public IActionResult GetResultHistory(HistoryRequest request)
         {
-            if (string.IsNullOrEmpty(keyword))
+            if (string.IsNullOrEmpty(request.Keyword))
             {
                 Logger.LogWarning("Empty keyword");
                 return NoContent();
             }
 
+            return Ok(GetSingleHistory(request));
+        }
+
+        private RatingRecord[] GetSingleHistory(HistoryRequest request)
+        {
+            var tracker = manager.Resolve(request.Keyword, "Keyword");
+            return tracker.GetRatings(request.Hours).OrderByDescending(item => item.Date).ToArray();
+        }
+
+        [HttpPost("historyall")]
+        public IActionResult GetResultHistoryAll(HistoryRequest[] request)
+        {
+            
+
+            var tracker = manager.Resolve(request.Keyword, "Keyword");
+            return Ok(tracker.GetRatings(request.Hours).OrderByDescending(item => item.Date));
+        }
+
+        private TrackingResults RequestSingle(string keyword)
+        {
             var tracker = manager.Resolve(keyword, "Keyword");
-            return Ok(tracker.GetRatings(hours).OrderByDescending(item => item.Date));
+            TrackingResults result = new TrackingResults { Keyword = keyword };
+
+            int[] steps = { 24, 12, 6, 1 };
+            foreach (int step in steps)
+            {
+                result.Sentiment[$"{step}H"] = new TrackingResult
+                {
+                    Average = tracker.CalculateAverageRating(step),
+                    TotalMessages = tracker.Count(lastHours: step)
+                };
+            }
+
+            result.Total = tracker.Count(false);
+            return result;
         }
     }
 }
---
+++ src/Wikiled.Twitter.Monitor.Service/Requests/HistoryRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wikiled.Twitter.Monitor.Service.Requests
+{
+    public class HistoryRequest
+    {
+        [Required]
+        [FromRoute]
+        public string Keyword { get; set; }
+
+        [Required]
+        [FromRoute]
+        public  int Hours { get; set; }
+    }
+}
